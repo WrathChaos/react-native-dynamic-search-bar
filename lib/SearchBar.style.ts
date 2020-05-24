21733cf31e83f4cc89d0e31207b20766371e34f2
FreakyCoder <kurayogun@gmail.com>
2020-05-24 22:55:52 +0300

    Styling changes

--- lib/SearchBar.style.ts
+++ lib/SearchBar.style.ts
@@ -1,7 +1,7 @@
 import { Platform, ViewStyle, StyleSheet } from "react-native";
 import {
   ScreenWidth,
-  isIPhoneXFamily
+  isIPhoneXFamily,
 } from "@freakycoder/react-native-helpers";
 
 interface Style {
@@ -16,7 +16,7 @@ export function container(props: any) {
     height = 35,
     width = ScreenWidth * 0.93,
     backgroundColor = "#fff",
-    borderRadius = 10
+    borderRadius = 10,
   } = props;
 
   return {
@@ -25,7 +25,7 @@ export function container(props: any) {
     borderRadius,
     backgroundColor,
     paddingLeft: 8,
-    paddingRight: 8
+    paddingRight: 8,
   };
 }
 
@@ -38,16 +38,16 @@ export function textInputStyle(
       android: {
         padding: 0,
         margin: 0,
-        borderWidth: 0
+        borderWidth: 0,
       },
       ios: {
-        bottom: 1
-      }
+        bottom: 1,
+      },
     }),
     marginLeft: 24,
     color: fontColor,
     fontSize: fontSize,
-    width: ScreenWidth * 0.7
+    width: ScreenWidth * 0.7,
   };
 }
 
@@ -58,17 +58,17 @@ export function _shadowStyle(shadowColor: string = "#757575") {
         shadowColor,
         shadowRadius: 5,
         shadowOpacity: 0.3,
-        shadowOffset: { width: 2, height: 1 }
+        shadowOffset: { width: 2, height: 1 },
       },
-      android: { elevation: 5 }
-    })
+      android: { elevation: 5 },
+    }),
   };
 }
 
 export function ifIPhoneXHeader(noExtraMargin: boolean) {
   if (noExtraMargin) return { marginTop: 16 };
   return {
-    marginTop: isIPhoneXFamily() ? 44 : 16
+    marginTop: isIPhoneXFamily() ? 44 : 16,
   };
 }
 
@@ -76,20 +76,19 @@ export default StyleSheet.create<Style>({
   containerGlue: {
     flex: 1,
     flexDirection: "row",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
   },
   searchStyle: {
-    top: 2,
     flex: 1,
-    flexDirection: "row"
+    flexDirection: "row",
   },
   textInputContainer: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   center: {
     alignSelf: "center",
-    alignContent: "center"
-  }
+    alignContent: "center",
+  },
 });
